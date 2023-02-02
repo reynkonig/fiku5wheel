37a3f297278d6cee6a97889db28d86e65cc4e02d
reynkonig <1nd3x@tuta.io>
2023-02-03 00:48:39 +0800

    v0.2.5 trying to fix vercel build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [
-    "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
