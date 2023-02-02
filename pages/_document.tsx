0a35229b57d9165c95d3005a9235b4fa42eae307
reynkonig <1nd3x@tuta.io>
2023-02-03 02:14:53 +0800

    v0.3.1

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ru">
+      <Head>
+        <title>Срулетка</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
